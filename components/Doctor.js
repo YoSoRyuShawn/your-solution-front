cd8ef4942eca96352ebe3a926e81c8041e99175d
LaShawn <lts001@gmail.com>
2020-12-16 16:28:37 +0900

    logo and color changes

--- components/Doctor.js
+++ components/Doctor.js
@@ -3,22 +3,28 @@ import styles from "../styles/Doctor.module.css";
 export default function Doctor(props) {
   return (
     <div className={styles.doctor}>
-      <img className={styles.doctorimage} src={props.doctor.image} />
-      <div className={styles.doctorname}>Dr.{props.doctor.first_name}</div>
-      <div className={styles.doctorspecialty}>
-        Specialties:{props.doctor.specialty}
+      <div>
+        <img className={styles.doctorimage} src={props.doctor.image} />
       </div>
-      <div className={styles.doctorprice}>{props.doctor.price} Yen/session</div>
-      <button
-        className={styles.button}
-        onClick={() => {
-          //doctorsの配列から選ばれたdoctorの情報をindexを使って取り出す
-          props.setIndex(props.doctor.id - 1);
-          props.setChangeView(false);
-        }}
-      >
-        See more
-      </button>
+      <div>
+        <div className={styles.doctorname}>Dr. {props.doctor.first_name}</div>
+        <div className={styles.doctorspecialty}>
+          Specialties: {props.doctor.specialty}
+        </div>
+        <div className={styles.doctorprice}>
+          {props.doctor.price} Yen/session
+        </div>
+      </div>
+        <button
+          className={styles.button}
+          onClick={() => {
+            //doctorsの配列から選ばれたdoctorの情報をindexを使って取り出す
+            props.setIndex(props.doctor.id - 1);
+            props.setChangeView(false);
+          }}
+        >
+          See more
+        </button>
     </div>
   );
 }
