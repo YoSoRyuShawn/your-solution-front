6bcec7901963ea1724d208d407b74b4cf2327b51
LaShawn <lts001@gmail.com>
2020-12-16 19:47:28 +0900

    added transform to card hover

--- components/Doctor.js
+++ components/Doctor.js
@@ -12,19 +12,21 @@ export default function Doctor(props) {
           Specialties: {props.doctor.specialty}
         </div>
         <div className={styles.doctorprice}>
-          {props.doctor.price} Yen/session
+          {" "}
+          Session Fee:{" "}
+          {props.doctor.price}
         </div>
       </div>
-        <button
-          className={styles.button}
-          onClick={() => {
-            //doctorsの配列から選ばれたdoctorの情報をindexを使って取り出す
-            props.setIndex(props.index);
-            props.setChangeView(false);
-          }}
-        >
-          See more
-        </button>
+      <button
+        className={styles.button}
+        onClick={() => {
+          //doctorsの配列から選ばれたdoctorの情報をindexを使って取り出す
+          props.setIndex(props.index);
+          props.setChangeView(false);
+        }}
+      >
+        See more
+      </button>
     </div>
   );
 }
