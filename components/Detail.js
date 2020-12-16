b8dd2e1c98d652f22c71fc92cdba47f0387605c9
竹園隆聖 <ryusei.takezono@gmail.com>
2020-12-16 09:00:32 +0900

    Detail.js

---
+++ components/Detail.js
@@ -0,0 +1,28 @@
+import styles from "../styles/Details.module.css";
+
+export default function Detail(props) {
+  return (
+    <div className={styles.detail}>
+      <div className={styles.info}>
+        <div className={styles.fullname}>
+          Dr.{props.doctor.first_name} {props.doctor.last_name}
+        </div>
+        <img className={styles.image} src={props.doctor.img} />
+        <div className={styles.speciality}>{props.doctor.speciality}</div>
+        <div className={styles.desc}>{props.doctor.description} </div>
+      </div>
+      <div className={styles.pay}>
+        {/* <div className={styles.availability}></div> How we select date*/}
+        <div className={styles.price}>{props.doctor.price}</div>
+        <button
+          className={styles.button}
+          onClick={() => {
+            //go payment
+          }}
+        >
+          book
+        </button>
+      </div>
+    </div>
+  );
+}
