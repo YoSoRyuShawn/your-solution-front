07af8829edc56eb04fb00593f5252b98e6a9d29f
LaShawn <lts001@gmail.com>
2020-12-16 14:46:16 +0900

    removed footer

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home({ data }) {
           />
         )}
       </main>
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -55,7 +55,7 @@ export default function Home({ data }) {
           Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
