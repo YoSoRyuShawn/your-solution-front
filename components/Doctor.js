2e1c7b2a10fa1cc6ad66cbf75c411540b67fc3d9
竹園隆聖 <ryusei.takezono@gmail.com>
2020-12-16 09:02:16 +0900

    Doctor.js

--- components/Doctor.js
+++ components/Doctor.js
@@ -3,11 +3,12 @@ import styles from "../styles/Doctor.module.css";
 export default function Doctor(props) {
   return (
     <div className={styles.doctor}>
-      <img src={props.doctor.img} />
+      <img className={styles.doctorimage} src={props.doctor.img} />
       <div className={styles.doctorname}>Dr.{props.doctor.first_name}</div>
       <div className={styles.doctorspeciality}>
         Specialities:{props.doctor.speciality}
       </div>
+      <div className={styles.doctorprice}>{props.doctor.price}Yen/session</div>
       <button
         className={styles.button}
         onClick={() => {
