6e567ced4e47517237f485abdf17799adbe03e07
Soshi <SoshiK@users.noreply.github.com>
2021-02-22 15:46:46 +0900

    changed backend url

--- utils/data.js
+++ utils/data.js
@@ -2,20 +2,20 @@ import axios from "axios";
 
 export const getAlldoctors = async () => {
   const data = (
-    await axios.get("https://your-solution-back.herokuapp.com//api/doctors")
+    await axios.get("https://your-solution-back.herokuapp.com/api/doctors")
   ).data;
   return data;
 };
 
 export const invokeZoom = async (body) => {
-  const res = await axios.post("https://your-solution-back.herokuapp.com//zoomMeeting",body);
+  const res = await axios.post("https://your-solution-back.herokuapp.com/zoomMeeting",body);
   if(res.status === 200) {
     return res.data.body.join_url;
   }
 }
 
 export const sendEmail = async (body) => {
-  const res = await axios.post("https://your-solution-back.herokuapp.com//email",body);
+  const res = await axios.post("https://your-solution-back.herokuapp.com/email",body);
   if(res.status === 200) {
     return true;
   }
